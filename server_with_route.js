893d458bf099c393070887804a331cc6385a8e12
RooshRoosh <roosh90@mail.ru>
2016-04-18 13:56:22 +0500

    авторизация через куки

--- server_with_route.js
+++ server_with_route.js
@@ -2,15 +2,14 @@ var http = require('http'),
     template = require('swig'),
     pg = require('pg'),
     express = require('express'),
+    cookieParser = require('cookie-parser'),  // npm install cookie-parser
+    bodyParser = require('body-parser'), // npm install body-parser
     debug = require('debug')('todo-node-postgres:server');
 
 var router = express.Router();
 
-
-
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 var connectionString = "postgres://postgres:postgres@localhost:5432/ai_recipe";
-
-
 
 
 router.get('/', function(req, res) {
@@ -27,7 +26,7 @@ router.get('/', function(req, res) {
         }
 
         // SQL Query > Select Data
-        var query = client.query("SELECT * FROM film ORDER BY id DESC");
+        var query = client.query("SELECT * FROM movie ORDER BY id DESC");
 
         // Stream results back one row at a time
         query.on('row', function(row) {
@@ -41,7 +40,8 @@ router.get('/', function(req, res) {
                 tmpl(
                     {
                         'film_list': film_list,
-                        'title': 'Кино для всех'
+                        'title': 'Кино для всех',
+                        'username': getUsernameFromCookie(req)
                     }
                 )
             )
@@ -67,7 +67,7 @@ router.get('/film/:film_id', function(req, res){
         }
 
         // SQL Query > Select Data
-        var query = client.query("SELECT * FROM film WHERE id=" + id);
+        var query = client.query("SELECT * FROM movie WHERE id=" + id);
 
         // Stream results back one row at a time
         query.on('row', function(row) {
@@ -77,6 +77,7 @@ router.get('/film/:film_id', function(req, res){
                 tmpl(
                     {
                         'film': row,
+                        'username': getUsernameFromCookie(req)
                     }
                 )
             )
@@ -92,57 +93,58 @@ router.get('/film/:film_id', function(req, res){
 
 })
 
-router.post('/commend/add/:film_id', function(req, res){
-    var id = req.params.film_id,
-    film_list = [],
-    tmpl = template.compileFile('./templates/film_details.html');
-
-
-    pg.connect(connectionString, function(err, client, done) {
-        // Handle connection errors
-        if(err) {
-          done();
-          console.log(err);
-          return res.status(500).json({success: false, data: err});
-        }
-
-        // SQL Query > Select Data
-        var query = client.query("SELECT * FROM film WHERE id=" + id);
-
-        // Stream results back one row at a time
-        query.on('row', function(row) {
-
-            film_list.push(row);
-            return res.end(
-                tmpl(
-                    {
-                        'film': row,
-                    }
-                )
-            )
-        });
-
-        // After all data is returned, close connection and return results
-        query.on('end', function() {
-            client.end()
-
-        });
-
-    });
-
+router.get('/logout', function(req, res){
+    res.clearCookie('username');
+    res.redirect('/');
+    // Убираем куку
 })
 
+router.get('/login', function(req, res){
+    // Рендерим страницу для авторизации
+    var tmpl = template.compileFile('./templates/login.html')
+    return res.end(
+        tmpl({})
+    )
+})
+
+router.post('/login', urlencodedParser, function(req, res){  // 3 параметра!
+    // Вторым параметром передаём паресер параметров запроса
+    var name = req.body['username'] || null
+    if (name){
+        console.log(name)
+    // Ставим куку, добавляем пользователя в базу если его там нет
+        res.cookie('username', name, { maxAge: 900000 });
+        res.redirect('/');
+
+    } else {
+        res.redirect('/login');
+    }
+})
 
 var port = 8008,
     app = express();
+
 app.set('port', port)
 app.use('/', router)
+app.use(cookieParser())
+app.use(bodyParser.json());
 
 var server = new http.createServer(app)
 server.listen(port, '127.0.0.1');
 
 server.on('error', onError);
 server.on('listening', onListening);
+
+
+function getUsernameFromCookie(req){
+    if (!req.headers.cookie){return null}
+    var cookies = req.headers.cookie.split('&').map(function(e){return e.split('=')})
+    for (var i=0; i<cookies.length;i++){
+        if (cookies[i][0] == 'username'){
+            return cookies[i][1]
+        }
+    }
+}
 
 /**
  * Normalize a port into a number, string, or false.
