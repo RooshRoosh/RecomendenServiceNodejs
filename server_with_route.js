78e3614e3e12e5f0405ccca60c86fe7dba32acd3
RooshRoosh <roosh90@mail.ru>
2016-05-23 11:36:31 +0500

    обработка не бинарной оценки за фильм

--- server_with_route.js
+++ server_with_route.js
@@ -4,7 +4,8 @@ var http = require('http'),
     express = require('express'),
     cookieParser = require('cookie-parser'),  // npm install cookie-parser
     bodyParser = require('body-parser'), // npm install body-parser
-    debug = require('debug')('todo-node-postgres:server');
+    debug = require('debug')('todo-node-postgres:server'),
+    querystring = require('querystring');
 
 var router = express.Router();
 
@@ -15,7 +16,12 @@ var connectionString = "postgres://postgres:postgres@localhost:5432/ai_recipe";
 router.get('/', function(req, res) {
 
     var tmpl = template.compileFile('./templates/index.html'),
-        film_list = [];
+        film_list = [],
+        username = getUsernameFromCookie(req);
+
+    if (!username){
+        res.redirect('/login');
+    }
 
     pg.connect(connectionString, function(err, client, done) {
         // Handle connection errors
@@ -26,27 +32,36 @@ router.get('/', function(req, res) {
         }
 
         // SQL Query > Select Data
-        var query = client.query("SELECT * FROM movie ORDER BY id DESC");
 
-        // Stream results back one row at a time
-        query.on('row', function(row) {
-            film_list.push(row);
-        });
+        var user_query = client.query(
+            "select *  from users as u "+
+            "where u.name='"+username+"'"
+        )
 
-        // After all data is returned, close connection and return results
-        query.on('end', function() {
-            client.end()
-            return res.end(
-                tmpl(
-                    {
-                        'film_list': film_list,
-                        'title': 'Кино для всех',
-                        'username': getUsernameFromCookie(req)
-                    }
+        user_query.on('row', function(row){
+           user_id = row['id'];
+           var query = client.query(
+                'SELECT * FROM movie as m ' +
+                'LEFT JOIN likes as l ON l.movie_id = m.id and l.user_id=' + user_id
+           );
+           query.on('row', function(row) {
+                film_list.push(row);
+            });
+           query.on('end',function(){
+                return res.end(
+                    tmpl(
+                        {
+                            'film_list': film_list,
+                            'title': 'Кино для всех',
+                            'username': username
+                        }
+                    )
                 )
-            )
-        });
-
+           })
+        })
+        user_query.on('end', function(){
+           client.end()
+        })
     });
 
 });
@@ -67,12 +82,20 @@ router.get('/film/:film_id', function(req, res){
         }
 
         // SQL Query > Select Data
-        var query = client.query("SELECT * FROM movie WHERE id=" + id);
+
+        var query = client.query(
+//        "SELECT * FROM movie WHERE id=" + id
+        'SELECT m.id, m.title, m.year, m.rank, m.poster, ARRAY_TO_JSON(ARRAY_AGG(m2.*)) as sim_movies ' +
+        'FROM movie as m '+
+        'JOIN movie_distance AS md ON md.movie1_id = m.id '+ // AND m1.l1 >0
+        'JOIN movie AS m2 ON md.movie2_id = m2.id AND m2.id!=m.id '+
+        'WHERE m.id =' + id +
+        ' GROUP BY m.id;'
+        );
 
         // Stream results back one row at a time
         query.on('row', function(row) {
 
-            film_list.push(row);
             return res.end(
                 tmpl(
                     {
@@ -109,19 +132,198 @@ router.get('/login', function(req, res){
 
 router.post('/login', urlencodedParser, function(req, res){  // 3 параметра!
     // Вторым параметром передаём паресер параметров запроса
-    var name = req.body['username'] || null
+    var name = unescape(req.body['username']) || null
     if (name){
-        console.log(name)
     // Ставим куку, добавляем пользователя в базу если его там нет
-        res.cookie('username', name, { maxAge: 900000 });
-        res.redirect('/');
+
+        pg.connect(connectionString, function(err, client, done) {
+            var query = client.query(
+                "INSERT INTO users (name) VALUES ('"+name+"') "+
+                "ON CONFLICT ON CONSTRAINT name_unique DO NOTHING;"
+            )
+
+            query.on('end', function(){
+
+                client.end()
+                res.cookie('username', name, { maxAge: 900000 });
+                res.redirect('/');
+            })
+        })
 
     } else {
         res.redirect('/login');
     }
 })
 
-var port = 8008,
+
+router.get('/users/', function(req, res){
+    user_list = [],
+    tmpl = template.compileFile('./templates/user_list.html');
+
+
+    pg.connect(connectionString, function(err, client, done) {
+    // Handle connection errors
+        if(err) {
+          done();
+          console.log(err);
+          return res.status(500).json({success: false, data: err});
+        }
+
+        // SQL Query > Select Data
+        var query = client.query("SELECT * FROM users");
+
+        // Stream results back one row at a time
+        query.on('row', function(row) {
+
+            user_list.push(row);
+        });
+
+        // After all data is returned, close connection and return results
+        query.on('end', function() {
+
+            client.end()
+            return res.end(
+                tmpl(
+                    {
+                        'user_list': user_list,
+                        'username': getUsernameFromCookie(req)
+                    }
+                )
+            )
+        });
+
+    });
+})
+
+router.get('/users/:user_id', function(req, res){
+
+      var id = req.params.user_id,
+      film_list = [],
+      tmpl = template.compileFile('./templates/user_details.html');
+
+
+      pg.connect(connectionString, function(err, client, done) {
+        // Handle connection errors
+        if(err) {
+          done();
+          console.log(err);
+          return res.status(500).json({success: false, data: err});
+        }
+
+        // SQL Query > Select Data
+        var query = client.query(
+        "SELECT * FROM users AS u JOIN likes AS l ON l.user_id = u.id JOIN movie AS m ON m.id=l.movie_id WHERE u.id =" + id
+        );
+
+        // Stream results back one row at a time
+        query.on('row', function(row) {
+            film_list.push(row);
+        });
+
+        // After all data is returned, close connection and return results
+        query.on('end', function() {
+
+            return res.end(
+                tmpl(
+                    {
+                        'film_list': film_list,
+                        'target_user': film_list[0]['name'],
+                        'username': getUsernameFromCookie(req)
+                    }
+                )
+            )
+            client.end()
+
+        });
+
+    });
+
+})
+
+
+router.get('/recommendation/users/', function(req, res){
+
+    var user_list = [],
+    username = getUsernameFromCookie(req)
+
+    tmpl = template.compileFile('./templates/recommendation_user.html');
+
+    pg.connect(connectionString, function(err, client, done) {
+
+     var query = client.query(
+        "select *  from users as u " +
+        "join user_distance as ud ON u.id = ud.user1_id " +
+        "join users as rec_user ON ud.user2_id = rec_user.id " +
+        "where l1 <> 0 and u.name='"+ username +
+        "' order by l2 DESC;"
+    );
+
+    query.on('row', function(row) {
+        user_list.push(row);
+    });
+
+    // After all data is returned, close connection and return results
+    query.on('end', function() {
+        return res.end(
+            tmpl(
+                {
+                    'user_list': user_list,
+                    'username': getUsernameFromCookie(req)
+                }
+            )
+        )
+        client.end()
+
+    });
+
+    })
+
+})
+
+router.post('/like', urlencodedParser, function(req, res){
+   var user = getUsernameFromCookie(req)
+   if (user){
+       var movie_id = unescape(req.body['id']) || null,
+           value = unescape(req.body['value']) || null;
+
+
+       pg.connect(connectionString, function(err, client, done) {
+
+           var user_query = client.query(
+            "select *  from users as u "+
+            "where u.name='"+user+"'"
+           )
+
+
+           console.log(1)
+
+
+           user_query.on('row', function(row){
+               user_id = row['id'];
+
+               var query = client.query(
+                   "INSERT INTO likes (user_id, movie_id, rating) VALUES ("+user_id+","+movie_id+","+value+") "+
+                   "ON CONFLICT ON CONSTRAINT likes_pkey DO UPDATE " +
+                   "SET rating="+value+" "+
+                   "WHERE likes.user_id="+user_id+" AND "+" likes.movie_id="+movie_id
+               )
+               query.on('end',function(){
+                    return res.end(
+                        'done!'
+                    )
+               })
+           })
+           user_query.on('end', function(){
+               client.end()
+           })
+       })
+   } else{
+        return res.end('error!')
+   }
+})
+
+
+var port = 8000,
     app = express();
 
 app.set('port', port)
@@ -141,7 +343,7 @@ function getUsernameFromCookie(req){
     var cookies = req.headers.cookie.split('&').map(function(e){return e.split('=')})
     for (var i=0; i<cookies.length;i++){
         if (cookies[i][0] == 'username'){
-            return cookies[i][1]
+            return querystring.unescape(cookies[i][1])
         }
     }
 }
@@ -205,3 +407,9 @@ function onListening() {
     : 'port ' + addr.port;
   debug('Listening on ' + bind);
 }
+
+
+
+
+
+
