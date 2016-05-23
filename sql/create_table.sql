c794f5e83770a3839c5b0e24e0fddadbeda572c7
RooshRoosh <roosh90@mail.ru>
2016-05-23 11:32:56 +0500

    Схема базы данных проекта

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -9,18 +9,19 @@ CREATE TABLE movie (
     rank real,
     position integer
 );
---CREATE TABLE users (
---    id serial PRIMARY KEY,
---    name text
---);
---CREATE TABLE comment (
---    id serial PRIMARY KEY,
---    user_id integer NOT NULL REFERENCES users(id),
---    movie_id integer NOT NULL REFERENCES movie(id),
---    text text
---);
---CREATE TABLE likes (
---    user_id integer NOT NULL REFERENCES users(id),
---    movie_id integer NOT NULL REFERENCES movie(id),
---    PRIMARY KEY(movie_id, user_id)
---)
+CREATE TABLE users (
+    id serial PRIMARY KEY,
+    name text
+);
+CREATE TABLE comment (
+    id serial PRIMARY KEY,
+    user_id integer NOT NULL REFERENCES users(id),
+    movie_id integer NOT NULL REFERENCES movie(id),
+    text text
+);
+CREATE TABLE likes (
+    user_id integer NOT NULL REFERENCES users(id),
+    movie_id integer NOT NULL REFERENCES movie(id),
+    rating real,
+    PRIMARY KEY(movie_id, user_id)
+)
